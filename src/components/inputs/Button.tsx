63711addf97cf4952b6d7e8611c5e3aa4ce7d289
Daniel Sayers <danielsayers7@gmail.com>
2021-07-05 16:52:07 +1200

    Add button input component

---
+++ src/components/inputs/Button.tsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames';
+import css from './Button.module.scss';
+
+type ButtonProps = Pick<React.ButtonHTMLAttributes<HTMLButtonElement>, 'type' | 'onClick' | 'disabled' | 'className'> & {
+  fullWidth?: boolean;
+  variant?: 'none' | 'primary';
+}
+
+const Button: React.FC<ButtonProps> = ({ children, fullWidth, onClick, type, disabled, variant, className }) => {
+  return (
+    <button
+      disabled={disabled}
+      type={type}
+      onClick={onClick}
+      className={classNames([
+        css.Button,
+        css[`Button--${variant}`],
+        {
+          [css['Button--fullWidth']]: fullWidth,
+        },
+        className,
+      ])}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  type: 'button',
+  variant: 'primary',
+};
+
+export default Button;
