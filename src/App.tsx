77ac0cdd394c4f13972ff6926f1752dbcb04f015
Daniel Sayers <danielsayers7@gmail.com>
2021-07-05 16:24:32 +1200

    Add routing and base css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import css from './App.module.scss';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import ManagementPath from './enums/ManagementPath';
+
 const App = () => {
   return (
-    <div>
-      Beb Pay
-    </div>
+    <BrowserRouter>
+      <div className={css.App}>
+        <Switch>
+          <Route path={ManagementPath.login}>
+            Login
+          </Route>
+          <Route path={ManagementPath.manage}>
+            Manage
+          </Route>
+          <Route>
+            Home
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
---
+++ src/enums/ManagementPath.ts
@@ -0,0 +1,17 @@
+enum ManagementPath {
+  manage = '/manage',
+  // Login
+  login = '/manage/login',
+  createAccount = '/manage/login/create-account',
+  forgotPassword = '/manage/login/forgot-password',
+  // Setup
+  branding = '/manage/branding',
+  products = '/manage/products',
+  experience = '/manage/experience',
+  details = '/manage/details',
+  webhooks = '/manage/webhooks',
+  monitors = '/manage/monitors',
+  launch = '/manage/launch',
+}
+
+export default ManagementPath;
