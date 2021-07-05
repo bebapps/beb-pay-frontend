a7f6046b7566c9d36187d7436106239c42f28e2b
Daniel Sayers <danielsayers7@gmail.com>
2021-07-06 07:52:26 +1200

    Add component for creating a QR code

---
+++ src/components/manage/QRCode.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useRef } from 'react';
+import QRCodeStyling from 'qr-code-styling';
+
+interface QRCodeProps {
+  size: number | `${number}%`;
+  color: string;
+  data: string;
+  imageUrl: string;
+}
+
+const QRCode: React.FC<QRCodeProps> = ({ size, color, data, imageUrl }) => {
+  const elRef = useRef<HTMLDivElement>(null);
+  const qrCodeRef = useRef<QRCodeStyling>();
+
+  const getAdjustedSize = () => {
+    if (typeof size === 'string') {
+      const percentage = Number(size.match(/\d*/)![0]);
+      const parentEl = elRef.current!.parentElement;
+      return Math.min(parentEl!.clientHeight, parentEl!.clientWidth) * percentage / 100;
+    } else {
+      return size;
+    }
+  };
+
+  const updateSize = () => {
+    const adjustedSize = getAdjustedSize();
+
+    qrCodeRef.current?.update({
+      width: adjustedSize,
+      height: adjustedSize,
+    });
+  };
+
+  useEffect(() => {
+    const adjustedSize = getAdjustedSize();
+
+    const qrCode = new QRCodeStyling({
+      width: adjustedSize,
+      height: adjustedSize,
+      cornersDotOptions: {
+        type: 'dot',
+      },
+      cornersSquareOptions: {
+        type: 'dot',
+      },
+      data: data,
+      image: imageUrl,
+      dotsOptions: {
+        color,
+        type: 'square',
+      },
+      qrOptions: {
+        errorCorrectionLevel: 'M',
+      },
+      imageOptions: {
+        crossOrigin: 'anonymous',
+        margin: 4,
+      },
+    });
+
+    qrCodeRef.current = qrCode;
+
+    elRef.current!.innerText = '';
+    qrCode.append(elRef.current!);
+
+    const resizeObserver = new ResizeObserver(updateSize);
+
+    resizeObserver.observe(elRef.current!.parentElement!);
+
+    return () => resizeObserver.disconnect();
+  }, []);
+
+  useEffect(() => {
+    updateSize();
+  }, [size]);
+
+  return (
+    <div ref={elRef} />
+  );
+};
+
+export default QRCode;
