bc50f6bb5f953edd10d2452f607b3a0aaf53dbe4
Daniel Sayers <danielsayers7@gmail.com>
2021-07-06 19:07:36 +1200

    Add support for showing image in FileDropper input

--- src/components/inputs/FileDropper.tsx
+++ src/components/inputs/FileDropper.tsx
@@ -1,19 +1,34 @@
 import classNames from 'classnames';
-import { useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import css from './FileDropper.module.scss';
 
 interface FileDropperProps {
   label: string;
+  value?: Blob | string;
   onChange: (file?: File | null) => void;
 }
 
-const FileDropper: React.FC<FileDropperProps> = ({ label, onChange }) => {
+const FileDropper: React.FC<FileDropperProps> = ({ label, value, onChange }) => {
   // use count to accommodate child elements
   const [dragEnterCount, setDragEnterCount] = useState(0);
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const valueRef = useRef<HTMLImageElement>(null);
 
   const incrementDragEnterCount = () => setDragEnterCount(count => count + 1);
   const decrementDragEnterCount = () => setDragEnterCount(count => count - 1);
+
+  useEffect(() => {
+    if (value instanceof Blob) {
+      const valueSrc = URL.createObjectURL(value);
+      valueRef.current!.src = valueSrc;
+
+      return () => URL.revokeObjectURL(valueSrc);
+    }
+
+    if (value) {
+      valueRef.current!.src = value;
+    }
+  }, [value]);
 
   return (
     <label
@@ -36,9 +51,19 @@ const FileDropper: React.FC<FileDropperProps> = ({ label, onChange }) => {
         onChange={e => onChange(e.target.files?.[0])}
         ref={fileInputRef}
       />
-      Drag and drop your {label},
-      <br />
-      or click to select
+      <img
+        className={classNames(css.FileDropper__image, {
+          [css['FileDropper__image--hidden']]: !value,
+        })}
+        ref={valueRef}
+      />
+      {!value && (
+        <>
+          Drag and drop your {label},
+          <br />
+          or click to select
+        </>
+      )}
     </label>
   );
 };
