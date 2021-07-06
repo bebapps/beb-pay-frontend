6a3e6e69369eeba40d7ff6efe07646047b0d8e16
Daniel Sayers <danielsayers7@gmail.com>
2021-07-06 19:45:59 +1200

    Add prop to disable Toggle input component

--- src/components/inputs/Toggle.tsx
+++ src/components/inputs/Toggle.tsx
@@ -4,15 +4,17 @@ import css from './Toggle.module.scss';
 interface ToggleProps {
   value: boolean;
   onChange: (value: boolean) => void;
+  disabled?: boolean;
 }
 
-const Toggle: React.FC<ToggleProps> = ({ value, onChange }) => (
+const Toggle: React.FC<ToggleProps> = ({ value, onChange, disabled }) => (
   <label className={css.Toggle}>
     <input
       className={css.Toggle__input}
       type="checkbox"
       checked={value}
       onChange={() => onChange(!value)}
+      disabled={disabled}
     />
     <span className={css.Toggle__slider} />
   </label>
