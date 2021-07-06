0e1d742f1482d47e70e6b835a94c15a19f9b14a5
Daniel Sayers <danielsayers7@gmail.com>
2021-07-06 23:12:58 +1200

    Add onBlur for ColorPicker input

--- src/components/inputs/ColorPicker.tsx
+++ src/components/inputs/ColorPicker.tsx
@@ -4,15 +4,17 @@ import css from './ColorPicker.module.scss';
 interface ColorPickerProps {
   value: string;
   onChange: (value: string) => void;
+  onBlur?: () => void;
 }
 
-const ColorPicker: React.FC<ColorPickerProps> = ({ value, onChange }) => (
+const ColorPicker: React.FC<ColorPickerProps> = ({ value, onChange, onBlur }) => (
   <label className={css.ColorPicker}>
     <input
       className={css.ColorPicker__input}
       type="color"
       value={value}
       onChange={e => onChange(e.target.value)}
+      onBlur={onBlur}
     />
     <div
       className={css.ColorPicker__color}
