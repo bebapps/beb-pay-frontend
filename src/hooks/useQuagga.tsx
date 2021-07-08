aad2a795ee9006df8e2a42bf6ed6ff8bf2894c36
Daniel Sayers <danielsayers7@gmail.com>
2021-07-08 20:22:30 +1200

    Add hook for barcode scanner

---
+++ src/hooks/useQuagga.tsx
@@ -0,0 +1,59 @@
+import Quagga, { QuaggaJSResultObject } from '@ericblade/quagga2';
+import { useRef, useState } from 'react';
+
+const startScanner = (target: HTMLDivElement) => {
+  Quagga.init({
+    frequency: 10,
+    inputStream: {
+      name: 'Live',
+      type: 'LiveStream',
+      target,
+      constraints: {
+        width: target.clientWidth,
+        height: target.clientHeight,
+        facingMode: 'environment',
+      },
+    },
+    decoder: {
+      readers: [
+        'code_128_reader',
+        'ean_reader',
+        'ean_8_reader',
+        'code_39_reader',
+        'code_39_vin_reader',
+        'codabar_reader',
+        'upc_reader',
+        'upc_e_reader',
+        'i2of5_reader',
+      ],
+    },
+
+  }, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    Quagga.start();
+  });
+};
+
+export const useQuagga = (onDetected: (result: QuaggaJSResultObject) => void) => {
+  const scannerTargetRef = useRef<HTMLDivElement>(null);
+  const [isScanning, setIsScanning] = useState(false);
+  Quagga.onDetected(onDetected);
+
+  const startScanning = () => {
+    if (!isScanning && scannerTargetRef.current) {
+      startScanner(scannerTargetRef.current);
+      setIsScanning(true);
+    }
+  };
+
+  const stopScanning = () => {
+    Quagga.stop();
+    setIsScanning(false);
+  };
+
+  return { scannerTargetRef, isScanning, startScanning, stopScanning };
+};
